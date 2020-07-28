ad7dd7792af1522e1bfedab1953a554ddcbeacab
Alexandre Tampieri <tampieri@bol.com.br>
2020-07-27 22:40:12 -0300

    Criação de Repositório

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Meu primeiro código PHP! <strong> Hello World! </strong> <br>" ;
+
+echo "Meu primeiro código PHP! <b> Hello World! </b> <br>";
+
+echo "Olá Mundo!!! <br> <br>";
+
+echo "Olá Terra!!!";
+
+?>
